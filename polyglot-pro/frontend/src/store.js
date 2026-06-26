a2bf81251017441607872696c6065aa8031b7968
ABHAY KUMAR <abhay11082005@gmail.com>
2026-06-26 09:59:47 +0530

    Add frontend Redux store configuration

---
+++ polyglot-pro/frontend/src/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit'
+import authReducer from './slices/authSlice'
+import courseReducer from './slices/courseSlice'
+import userReducer from './slices/userSlice'
+
+export default configureStore({
+  reducer: {
+    auth: authReducer,
+    courses: courseReducer,
+    user: userReducer,
+  },
+})
