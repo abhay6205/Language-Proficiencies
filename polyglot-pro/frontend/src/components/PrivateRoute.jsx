3345ee8e008ff1a27267cf81e08047c449ecd678
ABHAY KUMAR <abhay11082005@gmail.com>
2026-06-27 10:48:32 +0530

    feat(components): add PrivateRoute.jsx

---
+++ polyglot-pro/frontend/src/components/PrivateRoute.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
+function PrivateRoute() {
+  const { isAuthenticated } = useSelector((state) => state.auth)
+
+  return isAuthenticated ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/login" replace />
+  )
+}
+
+export default PrivateRoute
